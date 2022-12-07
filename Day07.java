466651adedf30938b0381b20559dc0134416e1e4
Bruce Backman <Bruce.Backman@msidata.com>
2022-12-07 05:28:46 -0600

    Day7 part 1 working

---
+++ Day07.java
@@ -0,0 +1,147 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Day07 {
+
+    public static void Run(List<String>input){
+        var fileSystem = new FileSystem();
+        ProcessInput(input,fileSystem);
+        
+        p1(fileSystem);
+    }
+
+    public static void p1(FileSystem filesystem)
+    {
+        var foundFolders = SubFoldersUpToSize( filesystem.getRoot(), 100000);
+        int totalSize =0;
+        for(var folder: foundFolders)
+            totalSize += folder.getTotalSize(true);
+        System.out.println("07.1: " + totalSize);
+    }
+    public static void ProcessInput(List<String>input, FileSystem fileSystem)
+    {
+        for(var line: input)
+        {
+            var tokens = line.split(" ");
+            if (tokens[0].compareTo( "$")==0)
+            {
+              if (tokens[1].compareTo("cd")==0 )
+                fileSystem.SetCurrent(tokens[2]);
+            }
+            else if (tokens[0].compareTo("dir")==0)
+            {
+                fileSystem.getCurrent().mkDir(tokens[1]);
+            }
+            else
+            {
+                fileSystem.getCurrent().createFile(tokens[1], Integer.parseInt(tokens[0]));
+            }
+        }
+    }
+    
+    public static List<Folder> SubFoldersUpToSize(Folder start, int maxSize)
+    {
+        var foundFolders = new ArrayList<Folder>();
+        for(var sub: start.getSubFolders())
+        {
+            for(var subFound : SubFoldersUpToSize(sub,maxSize))
+                foundFolders.add(subFound);
+            int subSize = sub.getTotalSize(true);
+            System.out.println("folder "+sub.getName()+" totalSize: "+subSize);
+            if (sub.getTotalSize(true) <= maxSize)
+                {
+                    System.out.println("folder "+sub.getName()+ " is at most " +maxSize);
+                    foundFolders.add(sub);
+                }
+        }
+        return foundFolders;
+    }
+    
+static class FileSystem{
+    Folder _root;
+    Folder _current;
+    
+    public FileSystem(){
+        _root = new Folder(null, "/");
+        _current = _root;
+    }
+    public Folder getRoot() {return _root;}
+    public Folder getCurrent(){return _current;}
+
+    public Folder SetCurrent(String folderName)
+    {
+        if (folderName.compareTo("/") == 0)
+            _current=_root;
+        else
+        {
+            var newFolder = _current.getFolder(folderName);
+            if (newFolder != null)
+                _current=newFolder;
+        }
+        return _current;
+    }
+}
+static class Folder{
+    String _name;
+    Folder _parent;
+    ArrayList<Folder> _subfolders;
+    ArrayList<File> _files;
+
+    public Folder(Folder parent, String name)
+    {
+        _name=name;
+        _parent=parent;
+        _subfolders=new ArrayList<Folder>();
+        _files = new ArrayList<File>();
+    }
+    public Folder getParent(){return _parent;}
+    public String getName(){return _name;}
+    public List<Folder> getSubFolders(){return _subfolders;    }
+    public List<File> getFiles(){return _files;}
+
+    public void mkDir(String name)
+    {
+        _subfolders.add(new Folder(this,name));
+    }
+    public void createFile(String name, int size)
+    {
+        _files.add(new File(name, size));
+    }
+    public Folder getFolder(String name)
+    {
+        if(name.compareTo("..")==0)
+            return _parent;
+        for(var sub: _subfolders)
+        {
+            if (sub.getName().compareTo(name)==0)
+            return sub;
+        }
+        return null;
+    }
+    public int getTotalSize(boolean recursive)
+    {
+        int size = 0;
+        for(var file: getFiles())
+            size += file.getSize();
+        if (recursive)
+        {
+            for(var sub: getSubFolders())
+                size += sub.getTotalSize(true);
+        }
+        return size;
+    }
+}
+static class File{
+    String _name;
+    int _size;
+    
+    public File(String name, int size)
+    {
+        _name=name;
+        _size= size;
+    }
+    public String getName(){return _name;}
+    public int getSize(){return _size;}
+}
+}
+
--- Main.java
+++ Main.java
@@ -9,10 +9,10 @@ class Main {
 
     public static void main(String[] args) throws java.io.IOException {
 
-    List<String> lines = Files.readAllLines(Paths.get("06_test.txt"));
+    List<String> lines = Files.readAllLines(Paths.get("07_data.txt"));
     //System.out.println("03.1: " + Day03.p1(lines));
     //System.out.println("03.2: " + Day03.p2(lines));
-    Day06.Run(lines);
+    Day07.Run(lines);
 
   }
 
