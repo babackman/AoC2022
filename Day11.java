e740f79c2d283d86aa98228e5d9d55474b78c29a
Bruce Backman <Bruce.Backman@msidata.com>
2022-12-11 16:09:34 -0600

    Day 11, converted to BigInt, p1 still working

--- Day11.java
+++ Day11.java
@@ -2,139 +2,194 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class Day11 {
-    public static void Run(List<String>input) {
-        Monkey[] monkeys = processInput(input);
-        p1(monkeys);
-    }
-    
-    private  static Monkey[] processInput(List<String>input){
-        // 7 input lines per monkey, except for the last which is 6
-        int monkeyCount=((input.size()-6) / 7)+1;
-        var monkeys = new Monkey[monkeyCount];
-        for(int i=0; i < monkeyCount; i++){
-            monkeys[i]=new Monkey();
-        }
-        
-        for(int i=0; i < monkeyCount; i++){
-            int startLine = i*7; // "Monkey 0:"
-            // +1 "  Starting items: 79, 98"
-            var items = input.get(startLine+1)
-                        .substring("  Starting items: ".length())
-                        .split(", ");
-            for(var item: items)
-                monkeys[i].addItem(Long.parseLong(item));
+    public static void Run(List<String> input) {
 
-            // +2 "  Operation: new = old * 19"
-            var operation = input.get(startLine+2)
-                        .substring("  Operation: new = old ".length());
+        p1(input);
+        //p2(input);
+        
+    }
+
+    private static Monkey[] processInput(List<String> input) {
+        // 7 input lines per monkey, except for the last which is 6
+        int monkeyCount = ((input.size() - 6) / 7) + 1;
+        var monkeys = new Monkey[monkeyCount];
+        for (int i = 0; i < monkeyCount; i++) {
+            monkeys[i] = new Monkey();
+        }
+
+        for (int i = 0; i < monkeyCount; i++) {
+            int startLine = i * 7; // "Monkey 0:"
+            // +1 " Starting items: 79, 98"
+            var items = input.get(startLine + 1)
+                    .substring("  Starting items: ".length())
+                    .split(", ");
+            for (var item : items)
+                monkeys[i].addItem(new BigInt(Long.parseLong(item)));
+
+            // +2 " Operation: new = old * 19"
+            var operation = input.get(startLine + 2)
+                    .substring("  Operation: new = old ".length());
             monkeys[i].setOperator(operation.charAt(0));
             if (!operation.substring(2).equals("old"))
-                monkeys[i].setConstOperand(Long.parseLong(operation.substring(2)));
+                monkeys[i].setConstOperand(Integer.parseInt(operation.substring(2)));
 
-            // +3 "  Test: divisible by 23"
-            monkeys[i].setTestDivisor(Long.parseLong(
-                input.get(startLine+3) .substring("  Test: divisible by ".length())));
+            // +3 " Test: divisible by 23"
+            monkeys[i].setTestDivisor(Integer.parseInt(
+                    input.get(startLine + 3).substring("  Test: divisible by ".length())));
 
-            // +4 "    If true: throw to monkey 2"
-            int targetIndex = Integer.parseInt(input.get(startLine+4)
-                .substring("    If true: throw to monkey ".length()));
+            // +4 " If true: throw to monkey 2"
+            int targetIndex = Integer.parseInt(input.get(startLine + 4)
+                    .substring("    If true: throw to monkey ".length()));
             monkeys[i].setTrueTarget(monkeys[targetIndex]);
 
-            // +5 "    If false: throw to monkey 3"
-            targetIndex = Integer.parseInt(input.get(startLine+5)
-                .substring("    If false: throw to monkey ".length()));
+            // +5 " If false: throw to monkey 3"
+            targetIndex = Integer.parseInt(input.get(startLine + 5)
+                    .substring("    If false: throw to monkey ".length()));
             monkeys[i].setFalseTarget(monkeys[targetIndex]);
         }
         return monkeys;
     }
 
-    private static void p1(Monkey[] monkeys){
-        // run 20 rounds 
-        for (int round = 0; round < 20; round++){
-            for(var monkey : monkeys)
+    private static void p1(List<String> input) {
+        var monkeys = processInput(input);
+        // run 20 rounds
+        for (int round = 0; round < 20; round++) {
+            for (var monkey : monkeys)
                 monkey.inspectAndThrowAll();
         }
 
         // get all inspection counts:
-        var inspectionCount = new int[monkeys.length];
-        for (int i=0; i < monkeys.length; i++)
-            inspectionCount[i]=monkeys[i].getInspectionCount();
+        var inspectionCount = new long[monkeys.length];
+        for (int i = 0; i < monkeys.length; i++)
+            inspectionCount[i] = monkeys[i].getInspectionCount();
 
         // multiply top 2 inspection counts:
         Arrays.sort(inspectionCount);
-        long monkeyBusiness = inspectionCount[inspectionCount.length-1] *inspectionCount[inspectionCount.length-2];
-        System.out.println("Day 11 p1: "+ monkeyBusiness);
+        BigInt monkeyBusiness = new BigInt(inspectionCount[inspectionCount.length - 1]);
+        monkeyBusiness.mul(inspectionCount[inspectionCount.length - 2]);
+        System.out.println("Day 11 p1: " + monkeyBusiness.toString());
     }
 
-    private static class Monkey{
+    private static void p2(List<String> input) {
+        String fiveBackspaces="\b\b\b\b\b";
+        var monkeys = processInput(input);
+        for (var monkey : monkeys)
+            monkey.setApplyReliefFactor(false);
+
+        // run 10000 rounds
+        for (int round = 1; round <= 10000; round++) {
+            System.out.print(fiveBackspaces+round);
+            for (var monkey : monkeys)
+                monkey.inspectAndThrowAll();
+            if ((round == 1) || (round==20) || ((round %1000)==0))
+            {
+                System.out.println(fiveBackspaces+"== After round "+ round + " ==");
+                for (int j=0;j < monkeys.length; j++)
+                    System.out.println("Monkey "+j+" inspected items "+monkeys[j].getInspectionCount()+" times.");
+            }
+        }
+
+        // get all inspection counts:
+        var inspectionCount = new int[monkeys.length];
+        for (int i = 0; i < monkeys.length; i++)
+            inspectionCount[i] = monkeys[i].getInspectionCount();
+
+        // multiply top 2 inspection counts:
+        Arrays.sort(inspectionCount);
+
+    }
+
+    private static class Monkey {
         // worry levels of items held by monkey
-        private ArrayList<Long> _items = new ArrayList<Long>();
+        private ArrayList<BigInt> _items = new ArrayList<BigInt>();
         // operator of the monkey's worry operation
         private char _operator;
-        // true if the opearand of the worry operation is _constOperand, if false, use old value
-        private boolean _useConstOperand=false;
+        // true if the opearand of the worry operation is _constOperand, if false, use
+        // old value
+        private boolean _useConstOperand = false;
         // constant operand to use if _useConstOperand is true
-        private long  _constOperand;
+        private int  _constOperand;
         // value use din divisible-by test for where to toss item
-        private long _testDivisor;
+        private int _testDivisor;
         // monkey to toss to if test evalueates to true
         private Monkey _trueTarget;
         // monkey to toss to if test evalueates to false
         private Monkey _falseTarget;
+        private static int _reliefFactor = 3;
+        // true if new item values should be divided by _reliefFactor
+        private boolean _applyReliefFactor = true;
         // number of items inspected by this monkey
-        private int _inspectionCount=0;
+        private int _inspectionCount = 0;
 
-        public void addItem(long item) {
+        public void addItem(BigInt item) {
             _items.add(item);
         }
-        public void setOperator(char operator){
+
+        public void setOperator(char operator) {
             _operator = operator;
         }
-        public void setConstOperand(long operand)
-        {
+
+        public void setConstOperand(int operand) {
             _constOperand = operand;
             _useConstOperand = true;
         }
-        public void setTestDivisor(long divisor){
-            _testDivisor=divisor;
+
+        public void setTestDivisor(int  divisor) {
+            _testDivisor = divisor;
         }
+
         public void setTrueTarget(Monkey target) {
             _trueTarget = target;
         }
+
         public void setFalseTarget(Monkey target) {
             _falseTarget = target;
         }
-        public int getInspectionCount(){
+
+        public void setApplyReliefFactor(boolean apply) {
+            _applyReliefFactor = apply;
+        }
+
+        public int getInspectionCount() {
             return _inspectionCount;
         }
 
-        public void inspectAndThrowAll()
-        {
-            for(var item : _items)
-            {
+        public void inspectAndThrowAll() {
+            for (var item : _items) {
                 _inspectionCount++;
-                var newValue = calculateNewItemValue(item);
-                var throwTo = ((newValue % _testDivisor) == 0)
-                    ? _trueTarget
-                    : _falseTarget;
-                throwTo.addItem(newValue);
+                updateItem(item);
+                var remainder = item.copy();
+                remainder.urem(_testDivisor);
+                var throwTo = (remainder.isZero())
+                        ? _trueTarget
+                        : _falseTarget;
+                throwTo.addItem(item);
             }
 
             // all items have been thrown:
             _items.clear();
         }
-        private long  calculateNewItemValue(long oldValue)
-        {long  operand = _useConstOperand ? _constOperand : oldValue;
-            long newValue;
-            if (_operator == '*')
-                newValue =  oldValue * operand;
-            else
-                newValue = oldValue + operand;
 
-            newValue =newValue / 3;
-            return newValue;
+        private void  updateItem(BigInt item){
+            if (_operator == '*'){
+                if(_useConstOperand)
+                    item.umul(_constOperand);
+                else
+                    item.karatsuba(item.copy()); // bigInt's weird name for multiplying by another BigInt
+
+            }
+            else{
+                if (_useConstOperand)
+                    item.uadd(_constOperand);
+                else
+                    item.add(item.copy());
+            }
+
+                
+        
+            if (_applyReliefFactor)
+                item.udiv(_reliefFactor);
         }
     }
 }
