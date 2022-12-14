c9948fe8f2d3eac1d637d876b2e0abb0e3980eaa
Bruce Backman <Bruce.Backman@msidata.com>
2022-12-14 07:03:22 -0600

    Day13 pt 2 working

--- Day13.java
+++ Day13.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.zip.DataFormatException;
 
@@ -14,15 +15,17 @@ public class Day13 {
         }
 
         p1(packets);
+        p2(packets);
     }
 
     private static void p1(List<List<Object>> packets)
     {
         int sum=0;
+        var packetComparator = new PacketComparator();
         for (int pair=1, pairs = packets.size()/2; pair<=pairs; pair++)
         {
             int index = (pair-1)*2;
-            if (CompareLists(packets.get(index), packets.get(index+1)) < 1)
+            if (packetComparator.compare(packets.get(index), packets.get(index+1)) < 1)
             {
                 // add 1-based index
                 sum += (pair);
@@ -31,43 +34,20 @@ public class Day13 {
 
         System.out.println("Day 13.1: " + sum);
     }
-    private static int CompareLists(List<Object> left, List<Object> right)
-    {
-        int rsize = right.size();
-        for (int i = 0, n = left.size(); i < n; i++)
-        {   
-            if (i>=rsize)
-                return +1; //right ran out first, left is bigger
-            var leftElement = left.get(i);
-            var rightElement = right.get(i);
-            int compare = CompareElements(leftElement, rightElement);
-            if (compare != 0)
-                return compare;
-        }
-        if (right.size() > left.size())
-            return -1; // left ran out first, left is smaller
 
-        return 0; // equal
-    }
-    private static int CompareElements(Object left, Object right)
+    private static void p2(List<List<Object>> packets)
     {
-        if (left instanceof Integer)
-        {
-            if (right instanceof Integer)
-            {
-                return ((Integer)left).compareTo((Integer)right);
-            }
-            // integer and list
-            return CompareLists(List.of(left), (List<Object>)right);
-        }
-        var leftList = (List<Object>)left;
-        if (right instanceof Integer)
-        {
-            return CompareLists(leftList, List.of(right));
-        }
-        return CompareLists(leftList, (List<Object>)right);
+        var marker1=new ListReader("[[2]]").getList();
+        var marker2=new ListReader("[[6]]").getList();
+        packets.add(marker1);
+        packets.add(marker2);
+        packets.sort(new PacketComparator());
+        
+        var index1 = packets.indexOf(marker1);
+        var index2 = packets.indexOf(marker2);
+        int decoderKey = (index1+1) * (index2+1);
+        System.out.println("Day 13.2: " + decoderKey);
     }
-    
     private static class ListReader{
         private int _index;
         private String _input;
@@ -131,4 +111,46 @@ public class Day13 {
             return _input.charAt(_index);
         }
     }
+
+    private static class PacketComparator implements Comparator<List<Object>>
+    {
+        
+        @Override
+        public int compare(List<Object> left, List<Object> right) {
+            int rsize = right.size();
+            for (int i = 0, n = left.size(); i < n; i++)
+            {   
+                if (i>=rsize)
+                    return +1; //right ran out first, left is bigger
+                var leftElement = left.get(i);
+                var rightElement = right.get(i);
+                int compare = CompareElements(leftElement, rightElement);
+                if (compare != 0)
+                    return compare;
+            }
+            if (right.size() > left.size())
+                return -1; // left ran out first, left is smaller
+    
+            return 0; // equal
+        }
+
+        private int CompareElements(Object left, Object right)
+        {
+            if (left instanceof Integer)
+            {
+                if (right instanceof Integer)
+                {
+                    return ((Integer)left).compareTo((Integer)right);
+                }
+                // integer and list
+                return compare(List.of(left), (List<Object>)right);
+            }
+            var leftList = (List<Object>)left;
+            if (right instanceof Integer)
+            {
+                return compare(leftList, List.of(right));
+            }
+            return compare(leftList, (List<Object>)right);
+        }
+        }
 }
